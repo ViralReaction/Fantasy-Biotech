fa21d38ae5d377af8a3efa712f98d10ace149c81
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-04-07 14:07:28 -0600

    Genetic Building Patching

---
+++ Source/Fantasy Biotech/Harmony/CompGenepackContainer_PowerOn.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace FantasyBiotech
+{
+    [HarmonyPatch(typeof(CompGenepackContainer), nameof(CompGenepackContainer.PowerOn), MethodType.Getter)]
+    public static class CompGenepackContainer_PowerOn
+    {
+        public static bool Prefix(CompGenepackContainer __instance, ref bool __result)
+        {
+            var thing = __instance.parent;
+            if (!cachedRefuelComps.TryGetValue(thing, out var comp))
+            {
+                comp = thing.TryGetComp<CompRefuelable>();
+                cachedRefuelComps[thing] = comp;
+            }
+            if (comp != null)
+            {
+                __result = comp.HasFuel;
+                return false;
+            }
+            return true;
+        }
+        private static readonly Dictionary<Thing, CompRefuelable> cachedRefuelComps = [];
+    }
+
+}
