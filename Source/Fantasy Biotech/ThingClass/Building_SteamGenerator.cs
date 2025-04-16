e1e3f13d4398fc4a5e234f202199271f77521ff9
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-04-15 18:28:10 -0600

    Steam Network

---
+++ Source/Fantasy Biotech/ThingClass/Building_SteamGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Verse;
+using PipeSystem;
+using System.Collections.Generic;
+using RimWorld;
+
+namespace FantasyBiotech
+{
+    public class Building_SteamGenerator : Building
+    {
+
+        private List<CompResourceTrader> traders = [];
+        public CompRefuelable compRefuelable;
+        private int tradersCount = 0;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            var comps = this.GetComps<CompResourceTrader>();
+            this.traders = new List<CompResourceTrader>(comps);
+            this.tradersCount = this.traders.Count;
+            this.compRefuelable = this.GetComp<CompRefuelable>();
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (compRefuelable != null)
+            {
+                if (compRefuelable.HasFuel)
+                {
+                    for (int index = 0; index < this.tradersCount; ++index)
+                    {
+                        this.BroadcastCompSignal(traders[index].OnSignal);
+                    }
+
+                }
+                else
+                {
+                    for (int index = 0; index < this.tradersCount; ++index)
+                    {
+                        this.BroadcastCompSignal(traders[index].OffSignal);
+                    }
+                }
+            }
+
+        }
+    }
+}
