ebc4ec5655249c6cbfbace367282d44ae2b0344c
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-04-17 18:55:49 -0600

    Steam Gestation Completion Wastepack Fix

---
+++ Source/FantasyBiotech/Harmony/BillMech_BillTick_Patch.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+using System.Linq;
+
+namespace FantasyBiotech
+{
+    [HarmonyPatch(typeof(Bill_Mech), "BillTick")]
+    public static class BillMech_BillTick_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var code = instructions.ToList();
+            var getGestator = AccessTools.PropertyGetter(typeof(Bill_Mech), "Gestator");
+            var originalCall = AccessTools.Method(typeof(Building_WorkTableAutonomous), "Notify_FormingCompleted");
+            var newMethod = AccessTools.Method(typeof(BillMech_BillTick_Patch), nameof(CustomNotify));
+
+            for (int i = 0; i < code.Count - 2; i++)
+            {
+                if (code[i].opcode == OpCodes.Ldarg_0 &&
+                    code[i + 1].Calls(getGestator) &&
+                    code[i + 2].Calls(originalCall))
+                {
+                    code[i + 2] = new CodeInstruction(OpCodes.Call, newMethod);
+                    break;
+                }
+            }
+
+            return code;
+        }
+
+        public static void CustomNotify(Building_MechGestator Gestator)
+        {
+            if (Gestator is Building_ConstructGestator gestator)
+            {
+                gestator.Notify_FormingCompleted();
+                return;
+            }
+            Gestator.Notify_FormingCompleted();
+        }
+    }
+
+}
