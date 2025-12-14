55f5a122a414947b47ff7fb2bc03cc3dee8d6754
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-12-13 20:09:18 -0700

    Royalty Hospitality Quest Integration
    * Now will do constructs with proper option on

--- Source/FantasyBiotech/QuestNodes/QuestNode_SpawnConstructCluster.cs
+++ Source/FantasyBiotech/QuestNodes/QuestNode_SpawnConstructCluster.cs
@@ -7,76 +7,76 @@ using Verse.Grammar;
 
 namespace FantasyBiotech
 {
-    public class QuestNode_SpawnConstructCluster : QuestNode
-{
-	[NoTranslate]
-	public SlateRef<string> inSignal;
-
-	[NoTranslate]
-	public SlateRef<string> tag;
-
-	public SlateRef<float?> points;
-
-	public SlateRef<IntVec3?> dropSpot;
-
-	public override void RunInt()
+	public class QuestNode_SpawnConstructCluster : QuestNode
 	{
-		Slate slate = QuestGen.slate;
-		QuestPart_ConstructCluster questPart_ConstructCluster = new QuestPart_ConstructCluster
+		[NoTranslate]
+		public SlateRef<string> inSignal;
+
+		[NoTranslate]
+		public SlateRef<string> tag;
+
+		public SlateRef<float?> points;
+
+		public SlateRef<IntVec3?> dropSpot;
+
+		public override void RunInt()
 		{
-			inSignal = QuestGenUtility.HardcodedSignalWithQuestID(inSignal.GetValue(slate)) ?? QuestGen.slate.Get<string>("inSignal"),
-			tag = QuestGenUtility.HardcodedTargetQuestTagWithQuestID(tag.GetValue(slate)),
-			mapParent = slate.Get<Map>("map").Parent,
-			sketch = GenerateSketch(slate),
-			dropSpot = dropSpot.GetValue(slate) ?? IntVec3.Invalid
-		};
-		QuestGen.quest.AddPart(questPart_ConstructCluster);
-		string text = "";
-		if (questPart_ConstructCluster.sketch.pawns != null)
-		{
-			text += PawnUtility.PawnKindsToLineList(questPart_ConstructCluster.sketch.pawns.Select((m) => m.kindDef), "  - ", ColoredText.ThreatColor);
-		}
-		string[] array = (from t in questPart_ConstructCluster.sketch.buildingsSketch.Things
-			where GenHostility.IsDefMechClusterThreat(t.def)
-			group t by t.def.label).Select(delegate(IGrouping<string, SketchThing> grp)
-		{
-			int num = grp.Count();
-			return num + " " + ((num > 1) ? Find.ActiveLanguageWorker.Pluralize(grp.Key, num) : grp.Key);
-		}).ToArray();
-		if (array.Any())
-		{
+			Slate slate = QuestGen.slate;
+			QuestPart_ConstructCluster questPart_ConstructCluster = new QuestPart_ConstructCluster
+			{
+				inSignal = QuestGenUtility.HardcodedSignalWithQuestID(inSignal.GetValue(slate)) ?? QuestGen.slate.Get<string>("inSignal"),
+				tag = QuestGenUtility.HardcodedTargetQuestTagWithQuestID(tag.GetValue(slate)),
+				mapParent = slate.Get<Map>("map").Parent,
+				sketch = GenerateSketch(slate),
+				dropSpot = dropSpot.GetValue(slate) ?? IntVec3.Invalid
+			};
+			QuestGen.quest.AddPart(questPart_ConstructCluster);
+			string text = "";
+			if (questPart_ConstructCluster.sketch.pawns != null)
+			{
+				text += PawnUtility.PawnKindsToLineList(questPart_ConstructCluster.sketch.pawns.Select((m) => m.kindDef), "  - ", ColoredText.ThreatColor);
+			}
+			string[] array = (from t in questPart_ConstructCluster.sketch.buildingsSketch.Things
+			                  where GenHostility.IsDefMechClusterThreat(t.def)
+			                  group t by t.def.label).Select(delegate(IGrouping<string, SketchThing> grp)
+			{
+				int num = grp.Count();
+				return num + " " + ((num > 1) ? Find.ActiveLanguageWorker.Pluralize(grp.Key, num) : grp.Key);
+			}).ToArray();
+			if (array.Any())
+			{
+				if (text != "")
+				{
+					text += "\n";
+				}
+				text += array.ToLineList(ColoredText.ThreatColor, "  - ");
+			}
 			if (text != "")
 			{
-				text += "\n";
+				QuestGen.AddQuestDescriptionRules(new List<Rule>
+				{
+					new Rule_String("allThreats", text)
+				});
 			}
-			text += array.ToLineList(ColoredText.ThreatColor, "  - ");
 		}
-		if (text != "")
+
+		private MechClusterSketch GenerateSketch(Slate slate)
 		{
-			QuestGen.AddQuestDescriptionRules(new List<Rule>
+			return ConstructClusterGenerator.GenerateClusterSketch(points.GetValue(slate) ?? slate.Get("points", 0f), slate.Get<Map>("map"));
+		}
+
+		public override bool TestRunInt(Slate slate)
+		{
+			if (!Find.Storyteller.difficulty.allowViolentQuests)
 			{
-				new Rule_String("allThreats", text)
-			});
+				return false;
+			}
+			if (MechUtility.ConstructFaction() == null)
+			{
+				return false;
+			}
+			return slate.Get<Map>("map") != null;
 		}
 	}
-
-	private MechClusterSketch GenerateSketch(Slate slate)
-	{
-		return ConstructClusterGenerator.GenerateClusterSketch(points.GetValue(slate) ?? slate.Get("points", 0f), slate.Get<Map>("map"));
-	}
-
-	public override bool TestRunInt(Slate slate)
-	{
-		if (!Find.Storyteller.difficulty.allowViolentQuests)
-		{
-			return false;
-		}
-		if (MechUtility.ConstructFaction() == null)
-		{
-			return false;
-		}
-		return slate.Get<Map>("map") != null;
-	}
-}
 
 }
