47d7f733a70dcdd35b0404aa2558b942a64fb8f2
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-12-29 17:18:32 -0700

    Fix Prisoner Gene Extraction

---
+++ Source/Code/FantasyBiotech/WorkGivers/WorkGiver_CarryToGeneRemover.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+namespace FantasyBiotech
+{
+    public class WorkGiver_CarryToGeneRemover : WorkGiver_CarryToBuilding
+    {
+        public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForDef(FantasyBiotechThingDefOf.VR_GeneRemover);
+
+        public override bool ShouldSkip(Pawn pawn, bool forced = false)
+        {
+            return !ModsConfig.BiotechActive;
+        }
+    }
+
+}
