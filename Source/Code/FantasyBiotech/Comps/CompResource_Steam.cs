2f7a9ee091110ae8bc3d3832fb5a8709e5209a21
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2026-01-02 18:35:48 -0700

    Steam Valve

---
+++ Source/Code/FantasyBiotech/Comps/CompResource_Steam.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using PipeSystem;
+using RimWorld;
+using Verse;
+
+namespace FantasyBiotech
+{
+    public class CompResource_Steam : CompResource
+    {
+
+        public override string CompInspectStringExtra()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (PipeNet == null)
+            {
+                sb.Append("PipeSystem_NotConnected".Translate(Resource.name));
+                return sb.ToString().Trim();
+            }
+
+            Resource res = Resource;
+            PipeNet net = PipeNet;
+            if (res.onlyShowStored)
+            {
+                sb.Append($"{"PipeSystem_Stored".Translate(res.name)} {net.Stored:##0} {res.unit}");
+            }
+            else
+            {
+                sb.Append("FantasyBiotech_SteamNetExcess".Translate((net.Production - net.Consumption).ToString("F0"), (net.Stored * 100 / GenDate.TicksPerDay).ToString("F1")));
+            }
+            if (DebugSettings.godMode)
+            {
+                sb.AppendInNewLine(net.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
